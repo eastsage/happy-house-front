01086b41f515d08ae49f5e47e6539840e17f00e8
이동현 <dpwk8@naver.com>
2023-05-22 17:48:46 +0900

    FEAT: databind 함수 완성

--- src/components/house/HouseGraph.vue
+++ src/components/house/HouseGraph.vue
@@ -1,111 +1,156 @@
 <template>
     <b-container v-if="housegraph" class="bv-example-row">
-      <b-row class="mb-2 mt-1">
-        <b-col><b-img :src="require('@/assets/apt.png')" fluid-grow></b-img></b-col>
-      </b-row>
-      <b-row>
-      </b-row>
-      <div class="toggle-bar">
-        <button @click="selectGraph('graph1')">Graph 1</button>
-        <button @click="selectGraph('graph2')">Graph 2</button>
-      </div>
-      <div class="chart-container" v-if="isChartContainerReady">
-        <canvas ref="chartCanvas" id="myChart" width="1200" height="300"></canvas>
-      </div>
+        <b-row class="mb-2 mt-1">
+            <b-col><b-img :src="require('@/assets/apt.png')" fluid-grow></b-img></b-col>
+        </b-row>
+        <b-row> </b-row>
+        <div class="toggle-bar">
+            <button @click="selectGraph('graph1')">Graph 1</button>
+            <button @click="selectGraph('graph2')">Graph 2</button>
+        </div>
+        <div class="chart-container" v-if="isChartContainerReady">
+            <canvas ref="chartCanvas" id="myChart" width="1200" height="300"></canvas>
+        </div>
     </b-container>
-  </template>
-  
-  <script>
-  import { mapState } from "vuex";
-  import { showBarChart} from "@/service/graph";
-  
-  const houseStore = "houseStore";
-  
-  export default {
+</template>
+
+<script>
+import { mapState } from "vuex";
+import { showBarChart } from "@/service/graph";
+
+const houseStore = "houseStore";
+
+export default {
     name: "HouseGraph",
     data() {
-      return {
-        selectedGraph: "graph1",
-        isChartContainerReady: false,
-        chart: null,
-      };
+        return {
+            selectedGraph: "graph1",
+            isChartContainerReady: false,
+            chart: null,
+        };
     },
     created() {
-      console.log(this.housegraph);
+        console.log(this.housegraph);
     },
     mounted() {
-      this.isChartContainerReady = true;
-      this.renderChart();
+        this.isChartContainerReady = true;
+        this.renderChart();
     },
     computed: {
-      ...mapState(houseStore, ["housegraph"]),
+        ...mapState(houseStore, ["housegraph"]),
     },
     filters: {
-      price(value) {
-        if (!value) return value;
-        return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      },
+        price(value) {
+            if (!value) return value;
+            return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        },
     },
     methods: {
-      selectGraph(selectedGraph) {
-        this.renderChart(selectedGraph);
-      },
-      renderChart(selectedGraph) {
-        const chartData = {
-          labels: ["A", "B", "C", "D"],
-          datasets: [],
-        };
-  
-        if (selectedGraph === "graph1") {
-          chartData.datasets.push({
-            label: "Dataset 1",
-            backgroundColor: "rgba(255, 99, 132, 0.2)",
-            borderColor: "rgba(255, 99, 132, 1)",
-            data: [10, 20, 30, 40],
-          });
-        }
-  
-        if (selectedGraph === "graph2") {
-          chartData.datasets.push({
-            label: "Dataset 2",
-            backgroundColor: "rgba(54, 162, 235, 0.2)",
-            borderColor: "rgba(54, 162, 235, 1)",
-            data: [50, 60, 70, 80],
-          });
-        }
+        databind() {
+            const wholeData = this.housegraph;
+            let month = [[], [], [], [], [], [], [], [], [], [], [], []];
+            for (let index = 0; index < wholeData.length; index++) {
+                let curMonth = wholeData[index].dealMonth;
+                let curDealAmount = wholeData[index].dealAmount.replace(/,/g, "");
+                if (curMonth == 1) {
+                    month[0].push(curDealAmount);
+                } else if (curMonth == 2) {
+                    month[1].push(curDealAmount);
+                } else if (curMonth == 3) {
+                    month[2].push(curDealAmount);
+                } else if (curMonth == 4) {
+                    month[3].push(curDealAmount);
+                } else if (curMonth == 5) {
+                    month[4].push(curDealAmount);
+                } else if (curMonth == 6) {
+                    month[5].push(curDealAmount);
+                } else if (curMonth == 7) {
+                    month[6].push(curDealAmount);
+                } else if (curMonth == 8) {
+                    month[7].push(curDealAmount);
+                } else if (curMonth == 9) {
+                    month[8].push(curDealAmount);
+                } else if (curMonth == 10) {
+                    month[9].push(curDealAmount);
+                } else if (curMonth == 11) {
+                    month[10].push(curDealAmount);
+                } else if (curMonth == 12) {
+                    month[11].push(curDealAmount);
+                }
+            }
+            let ans = [];
+            for (let i = 0; i < 12; i++) {
+                if (month[i].length > 0) {
+                    let sum = Number(0);
+                    for (let j = 0; j < month[i].length; j++) {
+                        sum += Number(month[i][j]);
+                    }
+                    console.log(sum);
+                    sum /= month[i].length;
+                    ans.push(sum);
+                } else {
+                    ans.push(0);
+                }
+            }
+            return ans;
+        },
+        selectGraph(selectedGraph) {
+            this.renderChart(selectedGraph);
+        },
+        renderChart(selectedGraph) {
+            /* 
+            1. chartData.datasets.push 를 담당하는 함수 만들기
+            2. chartData.datasets.remove를 담당하는 함수 만들기
+            3. label 값을 변수로 연도별 지정
+            4. destroy & render을 Push or Remove마다 해줌*/
+            const chartData = {
+                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                datasets: [],
+            };
+            this.databind();
+            if (selectedGraph === "graph1") {
+                chartData.datasets.push({
+                    label: "Dataset 1",
+                    backgroundColor: "rgba(255, 99, 132, 0.2)",
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    // data: [10, 20, 30, 40],
+                    data: this.databind(),
+                });
+            }
 
-            
-        const canvas = this.$refs.chartCanvas;
-        console.log("77",this.chart, canvas);
+            if (selectedGraph === "graph2") {
+                chartData.datasets.push({
+                    label: "Dataset 2",
+                    backgroundColor: "rgba(54, 162, 235, 0.2)",
+                    borderColor: "rgba(54, 162, 235, 1)",
+                    data: [50, 60, 70, 80],
+                });
+            }
+
+            const canvas = this.$refs.chartCanvas;
+            console.log("77", this.chart, canvas);
             if (canvas) {
                 if (this.chart) {
                     this.chart.destroy();
                     console.log(1);
                 }
-                
-                    console.log(2);
-                    const chart1 = showBarChart(
-                        "myChart",
-                        chartData.labels,
-                        chartData.datasets
-                  );
-                  this.chart = chart1;
-                  console.log("88",chart1); 
-              }
-            
-        
-      },
+
+                console.log(2);
+                const chart1 = showBarChart("myChart", chartData.labels, chartData.datasets);
+                this.chart = chart1;
+                console.log("88", chart1);
+            }
+        },
     },
-  };
-  </script>
-  
-  <style>
-  .toggle-bar {
+};
+</script>
+
+<style>
+.toggle-bar {
     margin-bottom: 20px;
-  }
-  
-  .chart-container {
+}
+
+.chart-container {
     height: 300px;
-  }
-  </style>
-  
+}
+</style>
--- src/service/graph.js
+++ src/service/graph.js
@@ -1,110 +1,25 @@
-// import Chart from 'chart.js'
+import Chart from "chart.js/auto";
 
-//         let myChart = null;
-//         let container = null;
+const showBarChart = function (chartId, labels, datasets) {
+    const ctx = document.getElementById(chartId);
+    if (!ctx) return; // 차트 컨테이너가 존재하지 않으면 함수 종료
 
-//         const show = function (event, num)
-//         {
-//           if (myChart)
-//             myChart.destroy()
-//           if(container)
-//             container.destroy()
-//           if (num == 1) {
-
-//             event.preventDefault();
-//             var context = document
-//               .getElementById('myChart')
-//               .getContext('2d');
-//             var m0 = 1
-//             var m1 = 2
-//             var m2 = 3
-//             var w0 = 4
-//             var w1 = 5
-//             var w2 = 6
-//             myChart = new Chart(context, {
-//               type: 'bar', // 차트의 형태
-//               data: { // 차트에 들어갈 데이터
-//                 labels: [
-//                   //x 축
-//                   "남성", '여성'
-//                 ],
-//                 datasets: [
-//                   {
-//                     label: "0등급",
-//                     data: [m0, w0], //1등급 10대 몇명, 20대 몇명, 30대 몇명 데이터입력
-//                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
-//                     borderColor: 'rgba(255, 99, 132, 1)',
-//                     pointBorderColor: "#fff",
-//                     pointBackgroundColor: "rgba(255, 99, 132, 1)",
-//                     borderWidth: 1,
-//                     fill: true
-//                   },
-//                   {
-//                     label: "1등급",
-//                     data: [m1, w1],
-//                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
-//                     borderColor: 'rgba(54, 162, 235, 1)',
-//                     borderWidth: 1,
-//                     pointBorderColor: "#fff",
-//                     pointBackgroundColor: "rgba(54, 162, 235, 1)",
-//                     fill: true
-//                   },
-//                   {
-//                     label: "2등급",
-//                     data: [m2, w2],
-//                     backgroundColor: 'rgba(255, 206, 86, 0.2)',
-//                     borderColor: 'rgba(255, 206, 86, 1)',
-//                     pointBorderColor: "#fff",
-//                     pointBackgroundColor: "rgba(255, 206, 86, 1)",
-//                     borderWidth: 1,
-//                     fill: true
-//                   }
-//                 ]
-//               },
-//               options: {
-//                 legend: { display: true },
-//                 title: {
-//                   display: true,
-//                   text: '성별 관측치 수'
-//                 },
-//                 scales: {
-//                   yAxes: [
-//                     {
-//                       ticks: {
-//                         beginAtZero: true
-//                       }
-//                     }
-//                   ]
-//                 }
-//               }
-//             })
-//           }
-// };
-// export default show
-
-import Chart from 'chart.js/auto';
-
-const showBarChart = function(chartId, labels, datasets) {
-  const ctx = document.getElementById(chartId);
-  if (!ctx) return; // 차트 컨테이너가 존재하지 않으면 함수 종료
-
-  return new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels,
-      datasets,
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        y: {
-          beginAtZero: true,
+    return new Chart(ctx, {
+        type: "line",
+        data: {
+            labels,
+            datasets,
         },
-      },
-    },
-  });
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
 };
 
 export { showBarChart };
-
