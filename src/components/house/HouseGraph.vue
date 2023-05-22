648107a1cb1666ee60f6501e39fd5b33fd5f25f7
김준현 <idkim8671@gmail.com>
2023-05-22 16:22:56 +0900

    그래프 드디어 바뀜 이제 이동현 집도

--- src/components/house/HouseGraph.vue
+++ src/components/house/HouseGraph.vue
@@ -1,61 +1,44 @@
 <template>
     <b-container v-if="housegraph" class="bv-example-row">
-        
-      
       <b-row class="mb-2 mt-1">
         <b-col><b-img :src="require('@/assets/apt.png')" fluid-grow></b-img></b-col>
       </b-row>
       <b-row>
-        
       </b-row>
       <div class="toggle-bar">
         <button @click="selectGraph('graph1')">Graph 1</button>
-      <button @click="selectGraph('graph2')">Graph 2</button>
-    </div>
-    <div class="chart-container" v-if="isChartContainerReady">
-      <canvas ref="chartCanvas" id="myChart" width="1200" height="300"></canvas>
-    </div>
-      
-    
-      
-      
+        <button @click="selectGraph('graph2')">Graph 2</button>
+      </div>
+      <div class="chart-container" v-if="isChartContainerReady">
+        <canvas ref="chartCanvas" id="myChart" width="1200" height="300"></canvas>
+      </div>
     </b-container>
   </template>
   
   <script>
   import { mapState } from "vuex";
-  import { showBarChart } from "@/service/graph";
-
+  import { showBarChart} from "@/service/graph";
+  
   const houseStore = "houseStore";
-  console.log(houseStore);
   
   export default {
     name: "HouseGraph",
-      data() {
-            return {
-                selectedGraph: "graph1",
-      isChartContainerReady: false,
-      chart: null,
-        }
+    data() {
+      return {
+        selectedGraph: "graph1",
+        isChartContainerReady: false,
+        chart: null,
+      };
     },
-      components: {
-        
+    created() {
+      console.log(this.housegraph);
     },
-      
-    
-     created() {
-        console.log(this.housegraph);    
-    },
-      mounted() {
-        this.isChartContainerReady = true;
-        this.renderChart();
+    mounted() {
+      this.isChartContainerReady = true;
+      this.renderChart();
     },
     computed: {
-          ...mapState(houseStore, ["housegraph"]),
-          
-      // house() {
-      //   return this.$store.state.house;
-      // },
+      ...mapState(houseStore, ["housegraph"]),
     },
     filters: {
       price(value) {
@@ -64,51 +47,65 @@
       },
     },
     methods: {
-    selectGraph(selectedGraph) {
-      this.renderChart(selectedGraph);
-    },
-    renderChart(selectedGraph = "graph1") {
-      const chartData = {
-        labels: ["A", "B", "C", "D"],
-        datasets: [
-          {
+      selectGraph(selectedGraph) {
+        this.renderChart(selectedGraph);
+      },
+      renderChart(selectedGraph) {
+        const chartData = {
+          labels: ["A", "B", "C", "D"],
+          datasets: [],
+        };
+  
+        if (selectedGraph === "graph1") {
+          chartData.datasets.push({
             label: "Dataset 1",
             backgroundColor: "rgba(255, 99, 132, 0.2)",
             borderColor: "rgba(255, 99, 132, 1)",
             data: [10, 20, 30, 40],
-          },
-        ],
-      };
+          });
+        }
+  
+        if (selectedGraph === "graph2") {
+          chartData.datasets.push({
+            label: "Dataset 2",
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            data: [50, 60, 70, 80],
+          });
+        }
 
-      if (selectedGraph === "graph2") {
-        chartData.datasets.push({
-          label: "Dataset 2",
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          data: [50, 60, 70, 80],
-        });
-      }
-
-      const canvas = this.$refs.chartCanvas;
-      if (canvas) {
-        if (this.chart) {
-          this.chart.destroy();
-            }
-            console.log(2);
-        this.chart = showBarChart("myChart", chartData.labels, chartData.datasets);
-      }
+            
+        const canvas = this.$refs.chartCanvas;
+        console.log("77",this.chart, canvas);
+            if (canvas) {
+                if (this.chart) {
+                    this.chart.destroy();
+                    console.log(1);
+                }
+                
+                    console.log(2);
+                    const chart1 = showBarChart(
+                        "myChart",
+                        chartData.labels,
+                        chartData.datasets
+                  );
+                  this.chart = chart1;
+                  console.log("88",chart1); 
+              }
+            
+        
+      },
     },
-  },
   };
   </script>
   
   <style>
-    .toggle-bar {
-  margin-bottom: 20px;
-}
-
-.chart-container {
-  height: 300px;
-}
-    </style>
+  .toggle-bar {
+    margin-bottom: 20px;
+  }
+  
+  .chart-container {
+    height: 300px;
+  }
+  </style>
   
